3446c1d2f64ee484cab8b8de27e0092c35a0ac57
Vaibhav Shukla <vaibhavshukla264@gmail.com>
2024-02-26 15:51:26 +0530

    changes

--- src/App.js
+++ src/App.js
@@ -10,7 +10,7 @@ function App() {
   return (
     <BrowserRouter basename="/e-commerce">
     <Routes>
-      <Route path="/" element={<Home/>} />
+      <Route exact path="/" element={<Home/>} />
       <Route path="/about" element={<About/>} />
       <Route path="/portfolio" element={<Portfolio/>} />
     </Routes>
