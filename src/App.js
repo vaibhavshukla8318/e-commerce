d0ef3bd01559776a69678d0c36ac856b8f587b3a
Vaibhav Shukla <vaibhavshukla264@gmail.com>
2024-02-26 16:27:03 +0530

    changes

--- src/App.js
+++ src/App.js
@@ -1,22 +1,17 @@
-
-import React from 'react'
-// import {BrowserRouter, Routes, Route} from 'react-router-dom'
-import Home from './component/Home'
-import Portfolio from './component/Portfolio'
-import About from './component/About'
-import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import React from 'react';
+import { Route, Routes } from 'react-router-dom';
+import Home from './component/Home';
+import Portfolio from './component/Portfolio';
+import About from './component/About';
 
 function App() {
   return (
-    <BrowserRouter basename="/e-commerce">
     <Routes>
-      <Route exact path="/" element={<Home/>} />
-     
-      <Route exact  path="/about" element={<About/>} />
-      <Route exact path="/portfolio" element={<Portfolio/>} />
+      <Route path="/" element={<Home />} />
+      <Route path="/about" element={<About />} />
+      <Route path="/portfolio" element={<Portfolio />} />
     </Routes>
- </BrowserRouter>
-  )
+  );
 }
 
-export default App
+export default App;
--- src/index.js
+++ src/index.js
@@ -1,13 +1,12 @@
 import React from 'react';
-import ReactDOM from 'react-dom/client';
+import { createRoot } from 'react-dom';
 import App from './App';
+import { BrowserRouter } from 'react-router-dom';
 
-
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
+createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <App />
+    <BrowserRouter basename='/e-commerce'>
+      <App />
+    </BrowserRouter>
   </React.StrictMode>
 );
-
-
